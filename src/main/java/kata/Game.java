aa6a7ef2fea01cd582c86c1435e5ee565243b041
jLereback <112405931+jLereback@users.noreply.github.com>
2023-01-30 16:33:53 +0100

    Add production code to make second test pass
    Refactor GameTest.java
    
    Add third case to parameterized test

--- src/main/java/kata/Game.java
+++ src/main/java/kata/Game.java
@@ -1,11 +1,12 @@
 package kata;
 
 public class Game {
+	private int score;
 	public void roll(int pinsKnockedDown) {
-
+		score += pinsKnockedDown;
 	}
 
 	public int score() {
-		return 8;
+		return score;
 	}
 }
--- src/test/java/kata/GameTest.java
+++ src/test/java/kata/GameTest.java
@@ -1,25 +1,24 @@
 package kata;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class GameTest {
 	Game game = new Game();
 
-	@Test
-	void twoRollsResultInScoreEqualToSum() {
-		game.roll(4);
-		game.roll(4);
-		assertThat(game.score()).isEqualTo(8);
+	@DisplayName("X rolls with Y pins knocked down result in expected score")
+	@ParameterizedTest(name = "{index} ==> {0} roll(s) with {1} pins down = {2} score")
+	@CsvSource({"2, 4, 8", "4, 3, 12", "3, 5, 20"})
+	void xRollsResultInExpectedScore(int numRolls, int pinsKnockedDown, int expected) {
+		rollMultiple(numRolls, pinsKnockedDown);
+		assertThat(game.score()).isEqualTo(expected);
 	}
 
-	@Test
-	void fourRollsResultInScoreEqualToSum() {
-		game.roll(3);
-		game.roll(3);
-		game.roll(3);
-		game.roll(3);
-		assertThat(game.score()).isEqualTo(12);
+	private void rollMultiple(int numRolls, int pinsKnockedDown) {
+		for (int i = 0; i < numRolls; i++)
+			game.roll(pinsKnockedDown);
 	}
 }
